a9478f42eab703403ccb4506175c9d3d14ec2b5c
FineJadeXavier <FineJadeXavier@Gmail.com>
2018-06-07 21:04:56 +0800

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,7 +25,20 @@
                     <td width="auto" align="left">
                         <div id="Search"><form action="https://www.google.com" onsubmit="return dispatch()" target="_blank"><div id="qbar"><input type="text" maxlength="40" name="q" id="q" value="" onfocus="$('#qbar').addClass('qbar_focus')" onblur="$('#qbar').removeClass('qbar_focus')"/></div></form></div>
                     </td>
-                    <td width="570" align="right" style="padding-top: 2px;"><a href="/" class="top">首页</a>&nbsp;&nbsp;&nbsp;<a href="/signup" class="top">注册</a>&nbsp;&nbsp;&nbsp;<a href="/signin" class="top">登录</a></td>
+                    @if(!session('id'))
+                    <td width="570" align="right" style="padding-top: 2px;">
+                        <a href="/" class="top">首页</a>&nbsp;&nbsp;&nbsp;
+                        <a href="/signup" class="top">注册</a>&nbsp;&nbsp;&nbsp;
+                        <a href="/signin" class="top">登录</a>
+                    </td>
+                    @else
+                    <td width="570" align="right" style="padding-top: 2px;">
+                        <a href="/" class="top">首页</a>&nbsp;&nbsp;&nbsp;
+                        <a href="/member/FineJadeXavier" class="top">FineJadeXavier</a>&nbsp;&nbsp;&nbsp;
+                        <a href="/settings" class="top">设置</a>&nbsp;&nbsp;&nbsp;
+                        <a href="#;" onclick="if (confirm('确定要从 V2EX 登出？')) { location.href= '/signout?once=27533'; }" class="top">登出</a>
+                    </td>
+                    @endif
                 </tr>
             </table>
         </div>
@@ -155,10 +168,16 @@
             <div class="sep20"></div>
             <strong>
                 <a href="/about" class="dark" target="_self">关于</a>
+                <span> · </span>
+                <a href="/about" class="dark" target="_self">反馈</a>
                 <div class="sep20"></div>
                 <p class="hitokoto" v-text="words"></p>
             </strong>
         </div>
+    </div>
+    <div class="sep20">
+    </div>
+    <div class="sep20">
     </div>
 </div>
 </body>
