b145ab6c4460cd82e6efa46a23a620657b68bfe2
lanmiaoCN <honglinbiao2017@outlook.com>
2018-06-07 20:57:59 +0800

    login&register

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    //登录
+    public function login()
+    {
+
+    }
+
+    //登录验证表单
+    public function p_login(Request $req)
+    {
+
+    }
+
+    //注册
+    public function register(){
+
+    }
+
+    //注册验证表单
+    public function p_register(Request $req)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('index.index');
 });
+
+//登录
+Route::get('/login', 'LoginController@login')->name('login.login');
+Route::post('/login', 'LoginController@post_login')->name('login.p_login');
+
+//注册
+Route::get('/register', 'LoginController@register')->name('login.register');
+Route::post('/register', 'LoginController@p_register')->name('login.p_register');
+
+
+//登录中间件
+Route::middleware('login')->group(function(){
+
+});
