f221634b09d5d030752771c5f9fd53120189a1b6
FineJadeXavier <FineJadeXavier@Gmail.com>
2018-06-08 07:56:23 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 //首页
 Route::get('/', function () { return view('index.index');});
 
 //登录
-Route::get('/signin', function () {
-    return view('user.signin');
-})->name('user.signin');
+Route::get('/signin', function () {return view('user.signin');})->name('user.signin');
 Route::post('/signin', 'UserController@signin')->name('user.signin.p');
 
 //注册
-Route::get('/signup', function () {
-    return view('user.signup');
-})->name('user.signup');
+Route::get('/signup', function () {return view('user.signup');})->name('user.signup');
 Route::post('/signup', 'UserController@signup')->name('user.signup.p');
 
 
