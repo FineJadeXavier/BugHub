b4d6f0db6c33211820294509ff29a68a7273f6aa
lanmiaoCN <honglinbiao2017@outlook.com>
2018-06-09 22:31:34 +0800

    13

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,6 @@ Route::post('/signin', 'UserController@signin')->name('user.signin.p');
 Route::get('/signup', function () {return view('user.signup');})->name('user.signup');
 Route::post('/signup', 'UserController@signup')->name('user.signup.p');
 
-
-
-//个人中心
-Route::get("/user/home/{nickname}", "UserController@home")->name('user.home');
-
 //文章详情（内容页）
 Route::get("/article/content/{id}", "TopicController@content")->name('article.content');
 
@@ -46,11 +41,13 @@ Route::get('/api/{type}/{orderby}/{order}',"TopicController@index_get" );
 //登录中间件
 Route::middleware('signin')->group(function(){
 
+    //个人中心
+    Route::get("/user/home/{nickname}", "UserController@home")->name('user.home');
+
     //编辑个人资料
     Route::get('/user/edit',function () {return view('user.edit');} )->name('user.edit');
     Route::post('/user/edit_p','UserController@edit_pwd')->name('user.edit.pwd');
     Route::post('/user/edit_e','UserController@edit_email')->name('user.edit.email');
-
 
     //编辑文章
     Route::get('/article/edit',"TopicController@edit")->name('article.edit');
